a46b325daa7ced7558e294aef6e68820f4f158dd
Magdalena Pawłowska <pawlowskam123@gmail.com>
2019-09-09 18:53:17 +0200

    Add checkbox

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -37,53 +37,36 @@ public class MyFirstTest {
             // Wpisz informacje do wyszukania
             name.sendKeys("Karol");
 
-            // Znajdź element wprowadzania tekstu na podstawie jego nazwy
             WebElement lastName = driver.findElement(By.id("last-name"));
-            // Wyczyść teskst zapisany w elemencie
             lastName.clear();
-            // Wpisz informacje do wyszukania
             lastName.sendKeys("Kowalski");
 
-            // Znajdź element wprowadzania tekstu na podstawie jego nazwy
+            WebElement gender = driver.findElement(By.name("gender"));
+            gender.click();
+            gender.sendKeys("Male");
+
             WebElement dob = driver.findElement(By.id("dob"));
-            // Wyczyść teskst zapisany w elemencie
             dob.clear();
-            // Wpisz informacje do wyszukania
             dob.sendKeys("05/22/2010");
 
-            // Znajdź element wprowadzania tekstu na podstawie jego nazwy
             WebElement address = driver.findElement(By.id("address"));
-            // Wyczyść teskst zapisany w elemencie
             address.clear();
-            // Wpisz informacje do wyszukania
             address.sendKeys("Prosta 51");
 
-            // Znajdź element wprowadzania tekstu na podstawie jego nazwy
             WebElement email = driver.findElement(By.id("email"));
-            // Wyczyść teskst zapisany w elemencie
             email.clear();
-            // Wpisz informacje do wyszukania
             email.sendKeys("karol.kowalski@mailinator.com");
 
-            // Znajdź element wprowadzania tekstu na podstawie jego nazwy
             WebElement password = driver.findElement(By.id("password"));
-            // Wyczyść teskst zapisany w elemencie
             password.clear();
-            // Wpisz informacje do wyszukania
             password.sendKeys("Pass123");
 
-            // Znajdź element wprowadzania tekstu na podstawie jego nazwy
             WebElement company = driver.findElement(By.id("company"));
-            // Wyczyść teskst zapisany w elemencie
             company.clear();
-            // Wpisz informacje do wyszukania
             company.sendKeys("Coders Lab");
 
-            // Znajdź element wprowadzania tekstu na podstawie jego nazwy
             WebElement comment = driver.findElement(By.id("comment"));
-            // Wyczyść teskst zapisany w elemencie
             comment.clear();
-            // Wpisz informacje do wyszukania
             comment.sendKeys("To jest mój pierwszy automat testowy");
 
             // Prześlij formularz
@@ -92,7 +75,7 @@ public class MyFirstTest {
         @After
         public void tearDown() throws Exception {
             // Zamknij przeglądarkę
-            driver.quit();
+            //driver.quit();
         }
     }
 
