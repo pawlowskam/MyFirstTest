1d11d1c839e0705b66d6433c0ed8997cfd41bc68
Magdalena Pawłowska <pawlowskam123@gmail.com>
2019-09-11 15:06:27 +0200

    Final corrections

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,10 +1,13 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class MyFirstTest {
 
 
@@ -65,17 +68,24 @@ public class MyFirstTest {
             company.clear();
             company.sendKeys("Coders Lab");
 
+            WebElement role = driver.findElement(By.id("role"));
+            Select roleSelect = new Select(role);
+            roleSelect.selectByVisibleText("QA");
+
             WebElement comment = driver.findElement(By.id("comment"));
             comment.clear();
             comment.sendKeys("To jest mój pierwszy automat testowy");
 
             // Prześlij formularz
             driver.findElement(By.id("infoForm")).submit();
+
+            WebElement successMessage = driver.findElement(By.id("submit-msg"));
+            Assert.assertEquals("Successfully submitted" , successMessage.getText());
         }
         @After
         public void tearDown() throws Exception {
             // Zamknij przeglądarkę
-            driver.quit();
+            //driver.quit();
         }
     }
 
